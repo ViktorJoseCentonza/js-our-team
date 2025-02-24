50d8022a0108417d2dc00f426d7e43ee5e4c9c29
ViktorJoseCentonza <viktorjose02@gmail.com>
2025-02-24 15:52:49 +0100

    add dynamic card generation

--- assets/script.js
+++ assets/script.js
@@ -3,36 +3,58 @@ const teamMembers = [
     name: "Marco Bianchi",
     role: "Designer",
     email: "marcobianchi@team.com",
-    img: "img/male1.png"
+    img: "./assets/img/male1.png"
   },
   {
     name: "Laura Rossi",
     role: "Front-end Developer",
     email: "laurarossi@team.com",
-    img: "img/female1.png"
+    img: "./assets/img/female1.png"
   },
   {
     name: "Giorgio Verdi",
     role: "Back-end Developer",
     email: "giorgioverdi@team.com",
-    img: "img/male2.png"
+    img: "./assets/img/male2.png"
   },
   {
     name: "Marta Ipsum",
     role: "SEO Specialist",
     email: "martarossi@team.com",
-    img: "img/female2.png"
+    img: "./assets/img/female2.png"
   },
   {
     name: "Roberto Lorem",
     role: "SEO Specialist",
     email: "robertolorem@team.com",
-    img: "img/male3.png"
+    img: "./assets/img/male3.png"
   },
   {
     name: "Daniela Amet",
     role: "Analyst",
     email: "danielaamet@team.com",
-    img: "img/female3.png"
+    img: "./assets/img/female3.png"
   }
 ];
+
+
+const cardsEl = document.querySelector(`#cards .row`)
+
+console.log(cardsEl);
+
+
+for (let i = 0; i < teamMembers.length; i++) {
+
+  cardsEl.innerHTML += `<div class="card d-flex">
+                    <div class="img-container">
+                        <img src="${teamMembers[i].img}" alt="">
+                    </div>
+                    <div class="card-content d-flex">
+                        <h2>${teamMembers[i].name}</h2>
+                        <p>${teamMembers[i].role}</p>
+                        <a href="#">${teamMembers[i].email}</a>
+                    </div>
+
+                </div>`
+
+}
